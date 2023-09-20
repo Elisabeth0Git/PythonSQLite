a5e34508964fc193e7b077e3065f25d5fe11e64c
Albert Kirakosyan <albert69kirakosyan@gmail.com>
2023-09-20 11:20:38 +0400

    SQLite

--- mainsql.py
+++ mainsql.py
@@ -5,10 +5,6 @@ with sq.connect('saper.db') as con:
     cur = con.cursor()  #Cursor
 
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY,
-    name TEXT,
-    sex INTEGER,
-    old INTEGER,
-    score INTEGER
-    )""")
+    cur.execute('SELECT * FROM users')
+    result = cur.fetchall()
+    print(result)
