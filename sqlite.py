d51486104675c43bda1501696fb23ebc55e2a634
Albert Kirakosyan <albert69kirakosyan@gmail.com>
2023-09-21 14:10:28 +0400

    SQLite

---
+++ sqlite.py
@@ -0,0 +1,15 @@
+import sqlite3 as sq
+
+
+with sq.connect('cars.db') as con:
+    cur = con.cursor()
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS cars(
+                car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                model TEXT,
+                price INTEGER)''')
+    cur.execute("INSERT INTO cars VALUES(1, 'Audi', 52642)")
+    cur.execute("INSERT INTO cars VALUES(2, 'Mercedes', 57127)")
+    cur.execute("INSERT INTO cars VALUES(3, 'Skoda', 9000)")
+    cur.execute("INSERT INTO cars VALUES(4, 'Volvo', 29000)")
+    cur.execute("INSERT INTO cars VALUES(5, 'Bentley', 350000)")
